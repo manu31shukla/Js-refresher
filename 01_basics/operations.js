f4c7335d32446418b7f11f06cf2f9c17dae2e474
Manu Shukla <82714021+manu31shukla@users.noreply.github.com>
2024-03-14 18:23:31 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,30 @@
+//simple operations
+let n = 20
+let m = 10
+let addo = n + m
+let diffo = n - m
+let multo = n*m
+let divido = n/m
+let remaindo = n%m
+let exponetioation = n**m
+console.log(addo);
+console.log(diffo);
+console.log(multo);
+console.log(divido);
+console.log(remaindo);
+console.log(exponetioation);
+
+//output defined by rules
+console.log("1"+2+3);
+console.log(1+2+"3");
+
+console.log(+true); //output would be 1 only
+// console.log(true+); //will throw error
+
+//prefix postfix operator
+let a = 10
+console.log(a); //output would be 10
+console.log(++a); //11
+// console.log(a++); //10
+
+//all info in js mdm
